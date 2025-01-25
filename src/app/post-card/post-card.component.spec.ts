0bc7bfa2c830e0e6a80e36aa1a35a79a3efe7bb9
charlesribeiro <charles.r.ribeiro@gmail.com>
2025-01-25 10:58:58 -0300

    Create base tests

--- src/app/post-card/post-card.component.spec.ts
+++ src/app/post-card/post-card.component.spec.ts
@@ -14,10 +14,25 @@ describe('PostCardComponent', () => {
 
     fixture = TestBed.createComponent(PostCardComponent);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('post', {
+      title: 'Post title',
+      body: 'Post body'
+    })
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should display the post title', ()=> {
+    const title = fixture.nativeElement.querySelector('.post-title');
+    expect(title.textContent).toContain('Post title');
+  })
+
+  it('should display the post body', ()=> {
+    const title = fixture.nativeElement.querySelector('.post-body');
+    expect(title.textContent).toContain('Post body');
+  })
 });
--- src/app/post-card/post-card.component.ts
+++ src/app/post-card/post-card.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Post } from '../models/post';
 
 @Component({
   selector: 'app-post-card',
-  imports: [],
   templateUrl: './post-card.component.html',
   styleUrl: './post-card.component.scss'
 })
 export class PostCardComponent {
-
+  post = input.required<Post>();
 }
